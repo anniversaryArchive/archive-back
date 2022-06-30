c71a5bb06924c52db5218a78dfa0c276b194d5dc
kimyejin <7602kyj@e-mirim.hs.kr>
2022-06-30 10:50:26 +0900

    feat: mongodb 연결

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
 const app = express();
+const port = 3000;
 
 const indexRouter = require('./routes/index');
 const archiveRouter = require('./routes/archive');
 
+// password : alflarhkgkrrh1 (미림과학고1)
+const url = 'mongodb+srv://archive:alflarhkgkrrh1@archive.esqyg.mongodb.net/?retryWrites=true&w=majority';
+
 app.use('/', indexRouter);
 app.use('/archive', archiveRouter);
 
-const port = 3000;
-app.listen(port, function () {
-  console.log(`Server running ... port : ${port}`);
-});
+
+mongoose.connect(url).then(() => {
+      console.log("MongoDB Connect")
+  }).catch((err) => {
+      console.log(err)
+  });
+  
+  
+  app.listen(3000, () => {
+      console.log("Server is Connect!");
+  });

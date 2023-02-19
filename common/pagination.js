389933883179282ccbad5cbc3bbf6751fa9aa7f0
hyejin-1010 <hyepago@gmail.com>
2023-02-19 15:20:45 +0900

    Fix: Pagination Search Bug

--- common/pagination.js
+++ common/pagination.js
@@ -1,13 +1,14 @@
 exports.getFindDoc = function (filter) {
   if (!filter) { return {}; }
   const { fields, q, flds } = filter;
-  if (!fields || !q) { return {}; }
 
   const findDoc = {};
-  for (const field of fields) {
-    findDoc[field] = { $regex: q };
+  if (fields && q) {
+    findDoc['$or'] = fields.map((field) => {
+      return { [field]: { $regex: q } };
+    });
   }
-
+  
   if (flds) {
     for (const key of Object.keys(flds)) {
       findDoc[key] = flds[key];

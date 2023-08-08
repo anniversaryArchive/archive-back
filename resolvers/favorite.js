0a04043ae02bcf785548f150759e91a4082e3892
hyejin-1010 <hyepago@gmail.com>
2023-08-08 23:07:19 +0900

    Mod: FavoritePagination query, start end filtering 가능하게끔 구현

--- resolvers/favorite.js
+++ resolvers/favorite.js
@@ -1,3 +1,7 @@
+// 참고
+// https://joyful-development.tistory.com/21
+
+const ObjectId = require('mongodb').ObjectID;
 const Favorite = require('../models/favorite');
 const User = require('../models/user');
 const Group = require('../models/group');
@@ -6,32 +10,72 @@ const Archive = require('../models/archive');
 const { ApolloError } = require('apollo-server-express');
 
 const { getUserId } = require('../utils');
-const { getFindDoc } = require('../common/pagination');
+
+function initDate (date) {
+  date = new Date(date);
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setDate(date.getDate() + 1);
+  return date;
+}
 
 const favoriteResolvers = {
   Query: {
     /**
      * Favorite을 Pagination으로 가져온다.
      * - page(Int): 현재 페이지 (0부터 시작)
-     * - perPage(Int): 한 페이지에 보여줄 데이터 수 
+     * - perPage(Int): 한 페이지에 보여줄 데이터 수
      * - sortField(String): 데이터 정렬할 필드 이름
      * - sortOrder(Int): 1: 오름차순, -1: 내림차순
-     * - filter(FilterOption)
+     * - group
+     * - start
+     * - end
      */
     async FavoritePagination (_, args, context) {
       const userId = getUserId(context);
       const sortField = args.sortField || 'createdAt';
       const sortOrder = args.sortOrder || 1;
       const page = args.page || 0;
-      const filter = args.filter || {};
-      filter.flds = Object.assign({}, filter.flds, { user: userId });
-      const doc = getFindDoc(filter);
       try {
-        const data = await Favorite.find(doc)
-          .sort({ [sortField]: sortOrder })
-          .limit(args.perPage)
-          .skip(args.perPage * page)
-        const total = await Favorite.find(doc).countDocuments({});
+        const skip = args.perPage * page;
+        let pipelines = [
+          { '$addFields': { 'archiveId': { '$toObjectId': '$archive' } } },
+          {
+            '$lookup': {
+              'from': 'archives',
+              'localField': 'archive',
+              'foreignField': '_id',
+              'as': 'archiveInfo'
+            }
+          },
+          { '$unwind': '$archiveInfo' },
+          { '$match': { 'user': ObjectId(userId) } },
+        ];
+
+        if (args.group) {
+          pipelines.push({ '$match': { 'archiveInfo.group': ObjectId(args.group) } });
+        }
+        if (args.end) {
+          pipelines.push({ '$match': { 'archiveInfo.startDate': { '$lte': initDate(args.end) } } });
+        }
+        if (args.start) {
+          pipelines.push({ '$match': { 'archiveInfo.endDate': { '$gte': initDate(args.start) } } });
+        }
+
+        pipelines = [
+          ...pipelines,
+          { '$sort': { [sortField]: sortOrder } },
+          {
+            '$facet': {
+              data: [{ $skip: skip }, { $limit: args.perPage }],
+              total: [{ $count: 'count' }]
+            }
+          }
+        ];
+
+        const results = await Favorite.aggregate(pipelines);
+        const { data, total } = results[0];
         return { data, total };
       } catch (error) {
         console.log(error);
@@ -82,7 +126,7 @@ const favoriteResolvers = {
         const findFavorite = await Favorite.findOne(doc);
         if (findFavorite) { return findFavorite; }
         const findArchive = await Archive.findOne({ _id: archive });
-        if (!findArchive) { 
+        if (!findArchive) {
           throw new ApolloError('해당 카페(Archive)를 찾을 수가 없습니다.', 1003, {});
         }
         doc.group = findArchive.group;
